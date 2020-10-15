698990a26d192dbde530052b04eb37f2785f4f9c
Lauren Dove <elledoveinc@gmail.com>
2020-10-15 13:20:58 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,6 +1,12 @@
-let state = {count: 0};
+// We want our reducer to be able to manage our state. We will comment out our state being assigned to th object.
+//let state = {count: 0};
 
-function changeState(state, action){
+
+//here we just declaring state INSTEAD of assigning it. It will start off as undefined.
+let state;
+
+//  Then here, in changeState, we are setting our state to a default argument of {count:0}
+function changeState(state ={count:0}, action){
     switch (action.type) {
       case 'INCREASE_COUNT':
         return {count: state.count + 1}
@@ -17,3 +23,6 @@ function dispatch(action){
 function render(){
     document.body.textContent = state.count
 }
+ // This will make it so anytime the page is loaded it starts off with 0.
+ //This is the default of our switch case being activated. Returning the default state if action.type isnt 'INCREASE_COUNT'
+dispatch({ type: '@@INIT' }) 
